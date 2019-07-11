f7e47c1bb28c1a3ebb296f4da2e3ad8057a10239
xyjsdo <xuyingjiii@126.com>
2019-07-11 13:53:56 +0800

    new

--- 指针/14.二级指针的偏移/main.c
+++ 指针/14.二级指针的偏移/main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+//二级指针服务于指针数组的偏移
 int main()
 {
 	char b[5][10] = { "lilei","hanmeimei","manchester","chelsea","primer" };
+	//char* p[5];//指针数组的数组名里存的指针类型是二级指针
+	char** p;
+	char** p2;
+	char* tmp;
+	p = (char**)malloc(sizeof(char*) * 5);//申请的空间如果需要存储指针值就需要强转成二级指针
+
+	p2 = p;
+	int i, j;
+	int a[5] = { 3,8,5,2,4 };
+
+	for (i = 0; i < 5; i++)
+	{
+		p2[i] = b[i];
+	}
+
+
+	for (i = 4; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (strcmp(p2[j], p2[j + 1])>0)
+			{
+				tmp = p2[j + 1];
+				p2[j + 1] = p2[j];
+				p2[j] = tmp;
+			}
+		}
+	}
+	for (i = 0; i < 5; i++)
+	{
+		puts(p2[i]);
+	}
+	printf("--------------------------\n");
+	for (i = 0; i < 5; i++)
+	{
+		puts(b[i]);
+	}
+
+	system("pause");
+	//printf("")
 }
---
+++ 指针/函数指针/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	void(*p)();//函数指针，用于存储函数的入口地址
+
+}
